4384b242a662ba1adefcb3a16c82d3af0ad5a484
Coolguy <mobilemc022@gmail.com>
2026-09-19 16:34:49 +0700

    Add Android app module

---
+++ app/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.krispy.mobilelegacy"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.krispy.mobilelegacy"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    packaging {
+        jniLibs {
+            useLegacyPackaging = true
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
